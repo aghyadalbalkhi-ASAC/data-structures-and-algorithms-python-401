6e8c1adfc55515199e3a0319823c946789a00f5c
M Aghead Albakhe <aghyadalbalkhi@gmail.com>
2020-12-01 08:15:13 +0200

    other way to solve the problem and add some validation

--- data_structures_and_algorithms/challenges/array_shift/array_shift.py
+++ data_structures_and_algorithms/challenges/array_shift/array_shift.py
@@ -1,18 +1,46 @@
 import math
 
 def insertShiftArray(array,value):
-    lenOfArray= len(array)
-    indexOfInsert = math.ceil(lenOfArray/2)
-    print(indexOfInsert)
-    newArr =[]
-    index=0
-    for i in range(lenOfArray+1):
-        if i == indexOfInsert:
-            newArr.append(value)
-        else:
-            newArr.append(array[index])
-            index+=1
-    return newArr
+
+    #validation Test
+    if len(array)== 0 or len(array)==1:
+        array.append(value)
+        return array
+    else :
+        lenOfArray= len(array)
+        indexOfInsert = math.ceil(lenOfArray/2)
+        print(indexOfInsert)
+        newArr =[]
+        index=0
+        for i in range(lenOfArray+1):
+            if i == indexOfInsert:
+                newArr.append(value)
+            else:
+                newArr.append(array[index])
+                index+=1
+        return newArr
 
 arr=[4,8,15,23,42]
 print(insertShiftArray(arr,16))
+
+
+def insertShiftArray2(array,value):
+    #validation Test
+    if len(array)== 0 or len(array)==1:
+        array.append(value)
+        return array
+    else:
+        # lastEle = len(array)-1    Orginal Length before increse sizq
+        middel = math.ceil(len(array)/2) #define the Middel
+        lastIndex = len(array)      #last Index in the array after append ele(0)
+        array.append(0)         #increase the size
+        n =len(array)           # length of array 
+        while n>middel :        # it while stop at middel index
+            array[lastIndex]=array[lastIndex-1]             #swap values betweeb the ele and its pre
+            lastIndex-=1
+            n-=1
+        array[middel]=value
+        return array
+    
+    
+insertShiftArray2([4,8,15,23,42],16)
--- tests/challenges/test_array_shift.py
+++ tests/challenges/test_array_shift.py
@@ -6,3 +6,18 @@ def test_insertShiftArray():
     actual = insertShiftArray([4,8,15,23,42],16)
     expected = [4,8,15,16,23,42]
     assert actual == expected
+
+def test_insertShiftArray2():
+    actual = insertShiftArray2([4,8,15,23,42],16)
+    expected = [4,8,15,16,23,42]
+    assert actual == expected
+
+def test_insertShiftArrayEmpty():
+    actual = insertShiftArray2([],16)
+    expected = [16]
+    assert actual == expected
+
+def test_insertShiftArrayOneElement():
+    actual = insertShiftArray2([2],16)
+    expected = [2,16]
+    assert actual == expected
